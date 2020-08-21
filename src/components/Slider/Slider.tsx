38e60548a2305fbd45ef6313a74c398cf9c6b625
Campari229 <KennyKoin@gmail.com>
2020-08-21 19:02:22 +0300

    small slider fix

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,6 +6,7 @@ const arr = [1, 2, 3];
 
 export const Slider: React.FC = () => {
   const [tr, setTr] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(1);
 
   const l = () => {
     const slider = document.getElementById('slider');
@@ -13,6 +14,7 @@ export const Slider: React.FC = () => {
 
     if (width && tr < 0) {
       setTr(tr + width);
+      setActiveSlide(activeSlide - 1);
     }
   };
 
@@ -22,24 +24,50 @@ export const Slider: React.FC = () => {
 
     if (width && -tr < width * (arr.length - 1)) {
       setTr(tr - width);
+      setActiveSlide(activeSlide + 1);
+    }
+  };
+
+  const handleSliderMove = (id: number) => {
+    const slider = document.getElementById('slider');
+    const width = slider?.offsetWidth;
+
+    if (width) {
+      setTr(-(width * (id - 1)));
+      setActiveSlide(id);
     }
   };
 
   return (
-    <div className="slider">
-      <button className="slider__btn" type="button" onClick={l}>{'<'}</button>
-      <ul id="slider" className="slider__images">
-        {arr.map(item => (
-          <li className="slider__item" style={{ transform: `translateX(${tr}px)` }}>
-            <img
-              alt=""
-              src={`img/slider-img/slide${item}.png`}
-              className="slider__image"
+    <>
+      <div className="slider">
+        <div className="slider__wrapper">
+          <button className="slider__btn" type="button" onClick={l}>{'<'}</button>
+          <ul id="slider" className="slider__images">
+            {arr.map(item => (
+              <li className="slider__item" style={{ transform: `translateX(${tr}px)` }}>
+                <img
+                  alt=""
+                  src={`img/slider-img/slide${item}.png`}
+                  className="slider__image"
+                />
+              </li>
+            ))}
+          </ul>
+          <button className="slider__btn" type="button" onClick={r}>{'>'}</button>
+        </div>
+        <div className="slider__buttons">
+          {arr.map(item => (
+            <button
+              aria-label="slider-button"
+              type="button"
+              className={activeSlide === item ? 'slider__button  slider__button--active' : 'slider__button'}
+              id={`${item}`}
+              onClick={() => handleSliderMove(item)}
             />
-          </li>
-        ))}
-      </ul>
-      <button className="slider__btn" type="button" onClick={r}>{'>'}</button>
-    </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
